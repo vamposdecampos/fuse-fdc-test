16bb992c24092d70882f2a0e18b37d7e4ab25b5d
Alex Badea <vamposdecampos@gmail.com>
2015-03-29 20:13:08 +0300

    Add spectrum +3 I/O routines

--- Makefile
+++ Makefile
@@ -9,5 +9,8 @@ all:	$(TARGET).tap
 %.tap: %.c
 	$(CC) $(LDFLAGS) $(CFLAGS) -o $@ $<
 
+%.asm: %.c
+	$(CC) $(CFLAGS) -a -o $@ $<
+
 clean:
 	rm -f *.tap *.o zcc_opt.def
--- fdc_test.c
+++ fdc_test.c
@@ -1,6 +1,72 @@
 #include <stdio.h>
 
-main()
+/* Spectrum +3 */
+
+static void __FASTCALL__ fdc_write(unsigned char val)
 {
-	printf("%cHello world!\n",12);
+	#asm
+	ld a, l
+	ld bc, 0x3ffd
+	out (c), a
+	#endasm
+}
+
+static unsigned char __FASTCALL__ fdc_read(void)
+{
+	#asm
+	xor a
+	ld h, a
+	ld bc, 0x3ffd
+	in a, (c)
+	ld l, a
+	#endasm
+}
+
+static unsigned char __FASTCALL__ fdc_status(void)
+{
+	#asm
+	xor a
+	ld h, a
+	ld bc, 0x2ffd
+	in a, (c)
+	ld l, a
+	#endasm
+}
+
+static void __FASTCALL__ fdc_motor_on(void)
+{
+	#asm
+	ld bc, 0x1ffd
+	ld a, 0x0c
+	out (c), a
+	#endasm
+}
+
+static void __FASTCALL__ fdc_motor_off(void)
+{
+	#asm
+	ld bc, 0x1ffd
+	ld a, 0x04
+	out (c), a
+	#endasm
+}
+
+
+static void sense_drive_status(void)
+{
+	fdc_write(4);
+	fdc_write(0);
+	printf("status: 0x%02x\n", fdc_status());
+	printf("result: 0x%02x\n", fdc_read());
+}
+
+
+int main(void)
+{
+	printf("%cuPD765 / i8272 FDC tester\n\n",12);
+
+	fdc_motor_on();
+	sense_drive_status();
+	fdc_motor_off();
+	return 0;
 }

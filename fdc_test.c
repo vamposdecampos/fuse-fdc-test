42a543c64fb783eda1c6f8fb59e35f4d7ee16779
Alex Badea <vamposdecampos@gmail.com>
2015-04-07 23:49:39 +0300

    fdc_test: add TC non-MT test-cases for reads at EOT

--- fdc_test.c
+++ fdc_test.c
@@ -530,6 +530,43 @@ struct test tests[] = {
 	},
 	{
 		.cmd.rw = {
+			.code.raw	= CMD_MF | CMD_READ_DATA,
+			.eot		= 18,
+			.gpl		= 0x2a,
+			.dtl		= 0xff,
+			.sel.hds = 0,
+			.c = 2, .h = 0, .r = 18, .n = 1,
+		},
+		.data_len = 5,
+		.tc = 1,
+		.res.rw = {
+			.c = 3, .h = 0, .r =  1, .n = 1,
+		},
+		.cmd_len = sizeof(struct fdc_cmd_rw),
+		.res_len = sizeof(struct fdc_res_rw),
+		.res_names = rw_res_names,
+	},
+	{
+		.cmd.rw = {
+			.code.raw	= CMD_MF | CMD_READ_DATA,
+			.eot		= 18,
+			.gpl		= 0x2a,
+			.dtl		= 0xff,
+			.sel.raw = 1 << 2,
+			.c = 2, .h = 1, .r = 18, .n = 1,
+		},
+		.data_len = 5,
+		.tc = 1,
+		.res.rw = {
+			.c = 3, .h = 1, .r =  1, .n = 1,
+			.st0.raw = 1 << 2,
+		},
+		.cmd_len = sizeof(struct fdc_cmd_rw),
+		.res_len = sizeof(struct fdc_res_rw),
+		.res_names = rw_res_names,
+	},
+	{
+		.cmd.rw = {
 			.code.raw	= CMD_MT | CMD_MF | CMD_READ_DATA,
 			.eot		= 18,
 			.gpl		= 0x2a,
